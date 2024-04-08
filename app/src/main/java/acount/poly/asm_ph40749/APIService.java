a3a2c19577fd39b2da3f40c55fd7407dcfab344f
ASM_DANENTANG2 <minhktph40749@fpt.edu.vn>
2024-04-08 16:33:08 +0700

    Minh

--- app/src/main/java/acount/poly/asm_ph40749/APIService.java
+++ app/src/main/java/acount/poly/asm_ph40749/APIService.java
@@ -12,17 +12,17 @@ import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface APIService {
-    String DOMAIN = "http://192.168.0.107:3000/";
+    String DOMAIN = "http://192.168.1.12:3000/api/";
 
-    @GET("/api/list")
+    @GET("get-list-car")
     Call<List<SanphamModel>> getSanphams();
 
-    @POST("/api/add")
+    @POST("post-car")
     Call<SanphamModel> addSanpham(@Body SanphamModel sanpham);
 
-    @DELETE("/api/delete/{id}")
+    @DELETE("delete-car-by-id/{id}")
     Call<Void> deleteSanpham(@Path("id") String id);
 
-    @PUT("/api/update/{id}")
+    @PUT("update-car-by-id/{id}")
     Call<SanphamModel> updateSanpham(@Path("id") String id, @Body SanphamModel sanpham);
 }
